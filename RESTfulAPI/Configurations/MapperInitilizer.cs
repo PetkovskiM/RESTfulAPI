3c3b7ea9f4ead364a128cf50656ccf702a4602fc
Mile Petkovski <mile_petkovski@hotmail.com>
2021-10-26 11:40:19 +0200

    Added Database,AutoMapper,Unit Of Work and DTOs

---
+++ RESTfulAPI/Configurations/MapperInitilizer.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RESTfulAPI.Data;
+using RESTfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulAPI.Configurations
+{
+    public class MapperInitilizer : Profile
+    {
+        public MapperInitilizer()
+        {
+            CreateMap<Country, CountryDTO>().ReverseMap();
+            CreateMap<Country, CreateCountryDTO>().ReverseMap();
+            CreateMap<Hotel, HotelDTO>().ReverseMap();
+            CreateMap<Hotel, CreateHotelDTO>().ReverseMap();
+        }
+    }
+}
--- RESTfulAPI/Startup.cs
+++ RESTfulAPI/Startup.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RESTfulAPI.Configurations;
+using RESTfulAPI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,18 +29,25 @@ namespace RESTfulAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+             );
 
-            services.AddControllers();
             services.AddCors(o => {
                 o.AddPolicy("AllowAll", builder =>
                     builder.AllowAnyOrigin()
                     .AllowAnyMethod()
                     .AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(MapperInitilizer));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RESTfulAPI", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
